aa85b8352edce66e71e90924fc24742afc553bbb
Ploy Unchit <ployunchit0@gmail.com>
2022-10-19 22:45:17 -0500

    added weapons and moving

--- 2D-Game/Assets/AlienCharacters/PlayerController.cs
+++ 2D-Game/Assets/AlienCharacters/PlayerController.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Alien : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float jumpHeight = 5; 
+    public float moveSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)){
+         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+         
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            
+        }
     }
 }
